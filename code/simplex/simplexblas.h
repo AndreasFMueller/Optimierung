c0213c371966803c516b0c91b1ba01d3f53b4b95
Andreas Mueller <andreas.mueller@othello.ch>
2013-03-07 08:33:35 +0100

    add missing simplexblas

---
+++ code/simplex/simplexblas.h
@@ -0,0 +1,11 @@
+/*
+ * simplexblas.h -- BLAS backend for the Simplex Algorithm
+ * 
+ * (c) 2013 Prof Dr Andreas Mueller, Hochschule Rapperswil
+ */
+#ifndef _simplexblas_h
+#define _simplexblas_h
+
+extern int simplexblas_version;
+
+#endif /* _simplexblas_h */
